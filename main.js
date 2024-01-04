bde3971772990a32568f6b4981975d5a09aa2cb3
PIGSx <104284451+PIGSx@users.noreply.github.com>
2024-01-03 21:26:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+const h1 = document.getElementById("h1");
+const banner = document.getElementById("banner");
+const button = document.getElementById("button");
+
+const onScroll = () => {
+  const scrollPosition = window.scrollY;
+  if (scrollPosition > 150) {
+    banner.style.backgroundSize = "150%";
+    h1.style.opacity = 0;
+    h1.style.transform = "translate(0, -50px) scale(0.9)";
+    button.style.opacity = 0;
+    button.style.transform = "translate(0, -50px) scale(0.8)";
+  } else {
+    banner.style.backgroundSize = "180%";
+    h1.style.opacity = 1;
+    h1.style.transform = "translate(0, 0) scale(1)";
+    button.style.opacity = 1;
+    button.style.transform = "translate(0, 0) scale(1)";
+  }
+};
+
+document.addEventListener("scroll", onScroll);
+
+// gallery
+const videos = document.querySelectorAll('.video video');
+const images = document.querySelectorAll('.image img');
+
+videos.forEach(video => {
+  video.addEventListener('click', () => {
+    if (video.paused) {
+      video.classList.add('active');
+      video.play();
+    } else {
+      video.classList.remove('active');
+      video.pause();
+    }
+  });
+
+  video.addEventListener('mouseenter', () => {
+    if (video.classList.contains('active')) {
+      video.muted = false;
+    }
+  });
+
+  video.addEventListener('mouseleave', () => {
+    if (video.classList.contains('active')) {
+      video.muted = true;
+    }
+  });
+});
+
+images.forEach(image => {
+  image.addEventListener('click', () => {
+    image.classList.toggle('active');
+  });
+});
